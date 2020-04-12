6a54172c3eb6f318eefcd6bb32afee77d776b309
muhammad <mhdsemps@gmail.com>
2020-04-12 21:51:39 +0300

    lint 3

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,5 @@
 const covid19ImpactEstimator = (data) => {
-  if (data.periodType === 'months') { data.timeToElapse *= 30; } else if (data.periodType === 'weeks') { data.timeToElapse *= 7; }
+  if (data.periodType === 'weeks') { data.timeToElapse *= 7; } else if (data.periodType === 'months') { data.timeToElapse *= 30; }
   const severeImpact = {};
   const impact = {};
   // const estimation = {};
