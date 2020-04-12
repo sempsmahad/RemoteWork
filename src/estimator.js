26453fd7429beb3a9141d35c7825047347381849
muhammad <mhdsemps@gmail.com>
2020-04-12 10:50:21 +0300

    new chal 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,23 +1,4 @@
-const covid19ImpactEstimator = (data) => {
-  const data = {
-    region: {
-      name: 'Africa',
-      avgAge: 19.7,
-      avgDailyIncomeInUSD: 5,
-      avgDailyIncomePopulation: 0.71
-    },
-    periodType: 'days',
-    timeToElapse: 58,
-    reportedCases: 674,
-    population: 66622705,
-    totalHospitalBeds: 1380614
-  };
-
-  const output = {
-    data: {}, // the input data you got
-    impact: {}, // your best case estimation
-    severeImpact: {} // your severe case estimation
-  };
+const covid19ImpactEstimator = (data) => { 
 
   if (data.periodType === 'months') data.timeToElapse *= 30;
   if (data.periodType === 'weeks') data.timeToElapse *= 7;
@@ -25,12 +6,17 @@ const covid19ImpactEstimator = (data) => {
   let impact = {};
   let estimation = {};
   let capacity = 0.35;
-  impact.currentlyInfected = data.reportedCases * 10;
-  severeImpact.currentlyInfected = data.reportedCases * 50;
+
+  let currentMixim = obj=> obj.currentlyInfected = data.reportedCases * 10;
+  currentMixim(impact);
+
+  let severeCurrentMixim = obj=> obj.currentlyInfected = data.reportedCases * 50;
+  severeCurrentMixim(severeImpact);
+
   let factor = Math.floor(data.timeToElapse / 3);
 
   let infectMixin = (obj) =>
-    (obj.infectionsByRequestedTime = obj.currentlyInfected * 2 ** factor);
+    (obj.infectionsByRequestedTime = obj.currentlyInfected * (2 ** factor));
   infectMixin(impact);
   infectMixin(severeImpact);
 
@@ -48,6 +34,12 @@ const covid19ImpactEstimator = (data) => {
     (obj.dollarsInFlight = obj.infectionsByRequestedTime * obj.region.avgDailyIncomePopulation * obj.region.avgDailyIncomeInUSD * obj.timeToElapse);
     
     obj.hospitalBedsByRequestedTime>0?obj.hospitalBedsByRequestedTime:-1*obj.severeCasesByRequestedTime;
+
+    return {
+      data: {}, 
+      impact: {}, 
+      severeImpact: {} 
+    };
 };
 
 export default covid19ImpactEstimator;
